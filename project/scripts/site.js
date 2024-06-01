a2fd2556964b4c896293385975719791b4c2881a
alexmach10 <danmachpad@gmail.com>
2024-06-01 12:35:48 -0600

    week05_sorrections

--- project/scripts/site.js
+++ project/scripts/site.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
+document.addEventListener("DOMContentLoaded", function () {
+
     let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
 
     if ("IntersectionObserver" in window) {
-        let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-            entries.forEach(function(entry) {
+        let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
                 if (entry.isIntersecting) {
                     let lazyImage = entry.target;
                     lazyImage.src = lazyImage.dataset.src;
@@ -15,13 +15,13 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
 
-        lazyImages.forEach(function(lazyImage) {
+        lazyImages.forEach(function (lazyImage) {
             lazyImageObserver.observe(lazyImage);
         });
     } else {
         // Fallback for browsers that don't support IntersectionObserver
-        let lazyLoad = function() {
-            lazyImages.forEach(function(lazyImage) {
+        let lazyLoad = function () {
+            lazyImages.forEach(function (lazyImage) {
                 if (lazyImage.getBoundingClientRect().top < window.innerHeight && lazyImage.getBoundingClientRect().bottom > 0 && getComputedStyle(lazyImage).display !== "none") {
                     lazyImage.src = lazyImage.dataset.src;
                     lazyImage.classList.remove("lazy");
@@ -55,7 +55,7 @@ function showSection(sectionId) {
 function toggleMenu() {
     const navMenu = document.getElementById('nav-menu');
     const menuButton = document.getElementById('menu-button');
-    
+
     navMenu.classList.toggle('active');
     menuButton.classList.toggle('open');
 }
